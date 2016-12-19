33ad3395d826803163400e7dfca35b9009fc755c
oracle <oracle@atanasoff29.danielcastelao.org>
2016-12-19 09:18:07 +0100

    creo el metodo edad y nombre

--- src/lovivido/LoVivido.java
+++ src/lovivido/LoVivido.java
@@ -10,17 +10,16 @@ import javax.swing.JOptionPane;
 public class LoVivido {
 
 public static void main(String[] args) {
-String n;
-String e;
+
+
 
 int m;
 int d;
 int h;
 int a;
+String n = nombre();
+ String e = edad();
 
-n = JOptionPane.showInputDialog ("Escriba su nombre: ");
-e = JOptionPane.showInputDialog ("Escriba su edad: ");
- 
 a = Integer.parseInt (e);
  
 m = (a*12);
@@ -32,6 +31,18 @@ h = (d*24);
    System.exit (0);
 
 }
+
+    public static String edad() throws HeadlessException {
+        String e;
+        e = JOptionPane.showInputDialog ("Escriba su edad: ");
+        return e;
+    }
+
+    public static String nombre() throws HeadlessException {
+        String n;
+        n = JOptionPane.showInputDialog ("Escriba su nombre: ");
+        return n;
+    }
 
     public static void Visualizar(int m, String n, int d, int h) throws HeadlessException {
         JOptionPane.showMessageDialog(null,  "Meses: "  +m,"Numero de meses vividos de " +n,JOptionPane.INFORMATION_MESSAGE);
